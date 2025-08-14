98fdeb2665f610cb5727f68edd686f68b3d22b16
manhquan <haibakhongtam2004@gmail.com>
2025-08-14 11:51:12 +0700

    Refs [TASK] [91211] [ADMIN] API filter danh sách đơn hàng

--- src/main/java/com/group7/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/group7/ecommerce/repository/OrderRepository.java
@@ -1,11 +1,12 @@
 package com.group7.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.group7.ecommerce.entity.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer>, JpaSpecificationExecutor<Order> {
 
 }
