50d88cbbb5aed405022de29bd964d0431b66deb7
Bipo04 <ledang1324@gmail.com>
2025-08-20 18:03:46 +0700

    them giao dien import san pham

--- src/main/java/com/group7/ecommerce/controller/admin/ProductController.java
+++ src/main/java/com/group7/ecommerce/controller/admin/ProductController.java
@@ -4,38 +4,57 @@ import com.group7.ecommerce.dto.request.ProductUpdateDto;
 import com.group7.ecommerce.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/products")
 public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("/import")
+    public String importProducts() {
+        return "/admin/products/import";
+    }
+
     @PostMapping("/import")
-    public ResponseEntity<?> importProducts(
+    public String importProducts(
             @RequestParam("excelFile") MultipartFile excelFile,
-            @RequestParam("imageZip") MultipartFile imageZip) {
-        productService.importFromExcelAndZip(excelFile, imageZip);
-        return ResponseEntity.ok("Import thành công");
+            @RequestParam("imageZip") MultipartFile imageZip,
+            RedirectAttributes redirectAttributes
+    ) {
+        try {
+            if (excelFile.isEmpty() || imageZip.isEmpty()) {
+                redirectAttributes.addFlashAttribute("error", "Vui lòng chọn đủ file Excel và Zip ảnh!");
+                return "redirect:/admin/products/import";
+            }
+            productService.importFromExcelAndZip(excelFile, imageZip);
+
+            redirectAttributes.addFlashAttribute("success", "Thêm thành công!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Có lỗi xảy ra: " + e.getMessage());
+        }
+
+        return "redirect:/admin/products/import";
     }
 
     @PutMapping(value = "/info/{id}")
-    public ResponseEntity<?> updateProduct(
+    public String updateProduct(
             @PathVariable Long id,
             @ModelAttribute @Valid ProductUpdateDto dto) {
         productService.updateProduct(id, dto);
-        return ResponseEntity.ok("Cập nhật sản phẩm thành công");
+        return "Cập nhật thành công";
     }
 
     @PutMapping(value = "/img/{id}")
-    public ResponseEntity<?> updateImageProduct(
+    public String updateImageProduct(
             @PathVariable Long id,
             @RequestParam(value = "images") MultipartFile[] images) {
         productService.updateImageProduct(id, images);
-        return ResponseEntity.ok("Cập nhật ảnh sản phẩm thành công");
+        return "Cập nhật ảnh sản phẩm thành công";
     }
 }
